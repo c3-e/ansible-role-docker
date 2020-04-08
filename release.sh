af0f87f367c4e6b7b685594953dda3815d8324b7
Amit Salunke <amit.salunke@c3iot.ai>
2020-04-07 20:38:54 -0700

    tmp - remove check for vendor

---
+++ release.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+VERSION=1.0.0
+PACKAGE_NAME=c3-docker
+
+package() {
+  tar -cvzf ${PACKAGE_NAME}-${VERSION}.tar.gz defaults files meta tasks LICENSE handlers
+  aws s3 cp ${PACKAGE_NAME}-${VERSION}.tar.gz s3://c3--packagemanager/ops/bootstrap/public/${PACKAGE_NAME}-${VERSION}.tar.gz --profile=c3.ci.aws
+}
+
+package
